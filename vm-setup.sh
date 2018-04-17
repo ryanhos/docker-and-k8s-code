61a4f1452b98eeb4a370257a15e9d4f41871ce9b
Ryan Hochstetler <ryan.hochstetler@getflywheel.com>
2018-04-17 02:53:50 -0500

    first commit

---
+++ vm-setup.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates wget software-properties-common
+wget https://download.docker.com/linux/debian/gpg
+sudo apt-key add gpg
+echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
+
