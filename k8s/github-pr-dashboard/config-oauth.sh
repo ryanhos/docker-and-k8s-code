d420e36d281c940ede1dfd47d6d851529c41fd2b
Ryan Hochstetler <ryan.hochstetler@getflywheel.com>
2018-05-30 16:36:48 -0500

    Add hostname config for config-oauth.sh

--- k8s/github-pr-dashboard/config-oauth.sh
+++ k8s/github-pr-dashboard/config-oauth.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+USER_SLUG=${USER//_/-}
+
 echo "Enter the client ID"
 read CLIENT_ID
 
@@ -11,3 +13,4 @@ COOKIE_SECRET=`python -c 'import os,base64; print base64.b64encode(os.urandom(16
 sed -i -e "s|##COOKIE_SECRET##|${COOKIE_SECRET}|g" add-oauth.yaml
 sed -i -e "s|##CLIENT_ID##|${CLIENT_ID}|g" add-oauth.yaml
 sed -i -e "s|##CLIENT_SECRET##|${CLIENT_SECRET}|g" add-oauth.yaml
+sed -i -e "s/ryan-hochstetler/${USER_SLUG}/g" add-oauth.yaml
