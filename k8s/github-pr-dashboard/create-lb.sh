94d78458fadb8309036eaae39d362ea9b634dfed
Ryan Hochstetler <ryan.hochstetler@getflywheel.com>
2018-05-30 03:04:20 -0500

    manually create LB

---
+++ k8s/github-pr-dashboard/create-lb.sh
@@ -0,0 +1,33 @@
+if [ -z "${1}" ]; then
+	echo "Please pass the GKE Instance name as the sole argument."
+	exit 1
+fi
+
+PROJECT_ID=round-ring-181705
+REGION=us-central1
+ZONE=us-central1-a
+USER_SLUG=${USER//_/-}
+INSTANCE_NAME=${1}
+DNS_ZONE=ryanh-org
+
+gcloud compute --project=${PROJECT_ID} addresses create $USER_SLUG --region=${REGION}
+
+gcloud compute target-pools create ${USER_SLUG}-github-pr-target-pool --region=${REGION} --http-health-check=k8s-a610d4e71d1dcc4e-node
+
+gcloud compute target-pools add-instances ${USER_SLUG}-github-pr-target-pool --region=${REGION} --instances=${INSTANCE_NAME}  --instances-zone=${ZONE}
+
+gcloud compute forwarding-rules create ${USER_SLUG}-forwarding-rule80 --address $USER_SLUG --ports=80 --target-pool=${USER_SLUG}-github-pr-target-pool --target-pool-region=${REGION} --region=${REGION}
+
+# Allow health checks to nginx ingress health port
+#gcloud compute firewall-rules create allow-health-check \
+#    --network default \
+#    --source-ranges 209.85.152.0/22,209.85.204.0/22,35.191.0.0/16 \
+#    --allow tcp:10254
+
+MY_IP=$(gcloud compute addresses describe ${USER_SLUG} --project=${PROJECT_ID} --format='value(address)' --region=${REGION})
+
+gcloud dns --project=${PROJECT_ID} record-sets transaction start --zone=${DNS_ZONE}
+gcloud dns --project=${PROJECT_ID} record-sets transaction add ${MY_IP} --name=${USER_SLUG}.ryanh.org. --ttl=300 --type=A --zone=${DNS_ZONE}
+gcloud dns --project=${PROJECT_ID} record-sets transaction execute --zone=${DNS_ZONE}
+
+echo "Your IP is: ${MY_IP}"
