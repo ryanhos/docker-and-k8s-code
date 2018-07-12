1e081d1f3cec3757186b4a9e67cb3609ed9cb97d
Ryan Hochstetler <ryan.hochstetler@getflywheel.com>
2018-07-12 07:56:02 -0500

    make gcp project name configurable

--- k8s/github-pr-dashboard/build.sh
+++ k8s/github-pr-dashboard/build.sh
@@ -4,6 +4,6 @@ docker build --build-arg COMMIT_SHA=df57d9d -t github-pr-dashboard .
 
 gcloud auth configure-docker
 
-docker tag github-pr-dashboard:latest us.gcr.io/round-ring-181705/github-pr-dashboard:latest
+docker tag github-pr-dashboard:latest us.gcr.io/${GOOGLE_CLOUD_PROJECT}/github-pr-dashboard:latest
 
-docker push us.gcr.io/round-ring-181705/github-pr-dashboard:latest
+docker push us.gcr.io/${GOOGLE_CLOUD_PROJECT}/github-pr-dashboard:latest
